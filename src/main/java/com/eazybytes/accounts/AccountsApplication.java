b85e06d39f6e1faf7c17e83260eb00ebd36d6dbb
jackrichter <jackrichter1@gmail.com>
2025-01-28 07:59:11 +0100

    Enhanced documentation using annotation @OpenApiDefinition.

--- src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.eazybytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+			title = "Accounts Microservice REST API Documentation",
+			description = "EazyBank Accounts microservice REST API Documentation",
+			version = "v1",
+			contact = @Contact(
+					name = "Madan Reddy",
+					email = "tutor@eazybytes.com",
+					url = "https://eazybytes.com"
+			),
+			license = @License(
+					name = "Appache 2.0",
+					url = "https://eazybites.com"
+			)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Accounts microservice REST API Documentation",
+				url = "https://eazybites.com/swagger-ui.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
